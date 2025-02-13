3abca34c1afba2b80e9fdf1d8ce54c22564e1e0d
cchaniotaki <cchaniot@usc.edu>
2025-02-12 19:34:44 -0800

    fixes for windows

--- core/src/main/java/com/crawljax/browser/WebDriverBackedEmbeddedBrowser.java
+++ core/src/main/java/com/crawljax/browser/WebDriverBackedEmbeddedBrowser.java
@@ -943,13 +943,10 @@ public final class WebDriverBackedEmbeddedBrowser implements EmbeddedBrowser {
 
         if (os.contains("win")) {
 
-            Long width = (Long) chromeDriver.executeScript("return document.body.scrollWidth");
-            Long height = (Long) chromeDriver.executeScript("return document.body.scrollHeight");
-
-            // Correctly handle devicePixelRatio as Double
-            Double scale = (Double) chromeDriver.executeScript("return window.devicePixelRatio");
-
-            int intscale = scale.intValue();
+            long width = (long) chromeDriver.executeScript("return document.body.scrollWidth");
+            long height = (long) chromeDriver.executeScript("return document.body.scrollHeight");
+            long scale = (long) chromeDriver.executeScript("return window.devicePixelRatio");
+            int intscale = (int) scale;
 
             HashMap<String, Object> setDeviceMetricsOverride = new HashMap<>();
             setDeviceMetricsOverride.put("deviceScaleFactor", scale);
